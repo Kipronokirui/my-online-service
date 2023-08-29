fbe53bd098665fac2ab5491bb679314476d6c1eb
Kipronokirui <victorkipronokirui2@gmail.com>
2023-08-30 00:09:36 +0300

    Adding pages

--- components/FAQs/QuestionsDetails.jsx
+++ components/FAQs/QuestionsDetails.jsx
@@ -1,9 +1,28 @@
+"use client";
 import React from 'react'
+import { Disclosure } from '@headlessui/react'
+// import { ChevronUpIcon } from '@heroicons/react/20/solid'
 
 export default function QuestionsDetails() {
   return (
-      <div>
-          QuestionsDetails
-      </div>
+    <div>
+      <section>
+        <div>
+          <Disclosure>
+            <div>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-green-200 px-4 py-3 mb-2 text-left font-medium text-green-600">
+                <span>
+                  This is the title
+                </span>
+              </Disclosure.Button>
+              <Disclosure.Panel static className="text-gray-500 flex w-full justify-between rounded-lg px-4 mb-2 text-left text-sm font-medium">
+                If you're unhappy with your purchase for any reason, email us
+                within 90 days and we'll refund you in full, no questions asked.
+              </Disclosure.Panel>
+            </div>
+          </Disclosure>
+        </div>
+      </section>
+    </div>
   )
 }
