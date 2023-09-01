65af224eabf9fa3be07ee435890540b5ab03ffcf
Kipronokirui <victorkipronokirui2@gmail.com>
2023-09-01 22:38:48 +0300

    Responsive features

--- components/FAQs/QuestionsDetails.jsx
+++ components/FAQs/QuestionsDetails.jsx
@@ -1,27 +1,38 @@
 "use client";
 import React from 'react'
 import { Disclosure } from '@headlessui/react'
+import { HiChevronDown } from "react-icons/hi";
+import {questions} from './utils'
 // import { ChevronUpIcon } from '@heroicons/react/20/solid'
 
 export default function QuestionsDetails() {
   return (
     <div>
-      <section>
-        <div>
+      <section className='mt-4'>
+          {questions.map((question, index) => (
+            <div key={index}>
           <Disclosure>
-            <div>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-green-200 px-4 py-3 mb-2 text-left font-medium text-green-600">
-                <span>
-                  This is the title
-                </span>
+            {({ open }) => (
+              <>
+                <Disclosure.Button className="flex space-x-3 w-full justify-between rounded-lg bg-green-200 px-4 py-3 mb-2 text-left font-medium text-green-600">
+                  {question.question_title}
+                  <span className={`ml-4 bg-white rounded-full`}>
+                    <HiChevronDown
+                      className={`h-8 w-8 text-purple-500 
+                        ${open ? 'rotate-180 transform' : ''}
+                        `}
+                    />
+                  </span>
               </Disclosure.Button>
-              <Disclosure.Panel static className="text-gray-500 flex w-full justify-between rounded-lg px-4 mb-2 text-left text-sm font-medium">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+              <Disclosure.Panel className="text-gray-500 flex w-full justify-between rounded-lg px-4 mb-2 text-left text-sm font-medium">
+                {question.answer}
               </Disclosure.Panel>
-            </div>
+              </>
+            )}
+              
           </Disclosure>
-        </div>
+          </div>
+          ))}
       </section>
     </div>
   )
--- components/HomePage/Hero.jsx
+++ components/HomePage/Hero.jsx
@@ -5,20 +5,23 @@ import styles from './home.module.css'
 export default function Hero() {
   return (
       <section className={`${styles.hero}`}>
-          <div className='grid max-w-screen-xl px-8 pt-16 mx-auto lg:gap-8 xl:gap-0 lg:py-24 lg:grid-cols-12'>
-              <div className='pl-16 mr-auto place-self-center lg:col-span-7'> 
+          <div className='container grid max-w-screen-xl lg:px-8 pt-10 lg:pt-16 mx-auto lg:gap-8 xl:gap-0 lg:py-24 lg:grid-cols-12'>
+              <div className='px-4 lg:pl-16 mr-auto place-self-center lg:col-span-7'> 
                   <h1 className='max-w-2xl mb-2 text-2xl font-extrabold text-white tracking-tight leading-none md:text-3xl xl:text-4xl dark:text-white'>
                       ACADEMIC WRITING SERVICE
                       DELIVERING THE BEST HELP ONLINE
                   </h1>
-                  <p className="max-w-2xl mb-6 font-light text-gray-200 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                  <p className="max-w-2xl mb-2 font-light text-gray-200 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                       Are you looking for an academic writer? Delve into our professional writing services online.
                       Opt-in for the best all-inclusive help with academic writing by industry-leading subject
                       virtuosos.
                   </p>
-                  <Link href={`#`} className='inline-flex items-center justify-center px-3 py-2 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800'>
-                      Get Started
-                  </Link>
+                  <div className='pb-2'>
+                    <Link href={`#`} className='inline-flex items-center justify-center px-3 py-2 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800'>
+                        Get Started
+                    </Link>
+                  </div>
+                  
               </div>
               {/* <h1>
                   Hero Section
